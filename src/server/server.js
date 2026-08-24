2b30950bd7cd8bbab99e6471da626f0f3329dc06
inclined4X <jezemiahsam48@gmail.com>
2026-08-24 12:28:49 +0000

    feat: add database connection and startup lifecycle

--- src/server/server.js
+++ src/server/server.js
@@ -3,11 +3,16 @@ const { port } = require("../config/env");
 const database = require("../database/database");
 
 const startServer = async function () {
-  await database();
+  try {
+    await database();
 
-  app.listen(port, () => {
-    console.log(`Express server is running on port ${port}`);
-  });
+    app.listen(port, () => {
+      console.log(`Express server is running on port ${port}`);
+    });
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 };
 
 startServer();
