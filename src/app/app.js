681e091a9bf81d3ce40284a514d3c4cae96659e9
inclined4X <jezemiahsam48@gmail.com>
2026-08-30 03:10:11 +0000

    move the erorhandler function to error handler.s middleware

--- src/app/app.js
+++ src/app/app.js
@@ -15,21 +15,5 @@ app.get("/test-error", (req, res, next) => {
   next(new AppError("Test error", 400));
 });
 
-const errorHandler = (err, req, res, next) => {
-  console.error(err.stack);
-
-  if (err instanceof AppError) {
-    return res.status(err.statusCode).json({
-      error: true,
-      message: err.message,
-    });
-  }
-
-  return res.status(500).json({
-    error: true,
-    message: "Internal Server Error",
-  });
-};
-
 app.use(errorHandler);
 module.exports = app;
---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,19 @@
+const AppError = require("../errors/appError");
+
+const errorHandler = (err, req, res, next) => {
+  console.error(err.stack);
+
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      error: true,
+      message: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    error: true,
+    message: "Internal Server Error",
+  });
+};
+
+module.exports = errorHandler;
