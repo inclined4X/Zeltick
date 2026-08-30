d49626fd684c7a2d806c90e23b5232e53c9cc0c9
inclined4X <jezemiahsam48@gmail.com>
2026-08-30 03:33:09 +0000

    doing some refractoring for a better architecture

--- src/app/app.js
+++ src/app/app.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const errorHandler = require("../middleware/errorHandler");
+const healthRoutes = require("../routes/healthRoutes");
 
 const app = express();
 app.use(express.json());
 
-app.get("/health", (req, res) => {
-  res.status(200).json({
-    status: "OK",
-    timestamp: Date.now(),
-  });
-});
+app.use("/", healthRoutes);
 
 app.use(errorHandler);
 module.exports = app;
