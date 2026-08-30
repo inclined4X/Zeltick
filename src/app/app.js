7cdcbb18830251d9f266bf9948ad56b8d3dae666
inclined4X <jezemiahsam48@gmail.com>
2026-08-30 02:45:05 +0000

    error handling middleware with an extended error for statuscode

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const AppError = require("../errors/appError");
 
 const app = express();
+app.use(express.json());
 
 app.get("/health", (req, res) => {
   res.status(200).json({
@@ -9,4 +11,19 @@ app.get("/health", (req, res) => {
   });
 });
 
+app.get("/test-error", (req, res, next) => {
+  next(new AppError("Test error", 400));
+});
+
+const errorHandler = (err, req, res, next) => {
+  console.error(err.stack);
+
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({
+    error: true,
+    message: err.message || "Internal Server Error",
+  });
+};
+
+app.use(errorHandler);
 module.exports = app;
