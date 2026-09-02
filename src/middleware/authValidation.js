6d3d45a3ec095bb1cf8b90e862c3af1f0e610885
inclined4X <jezemiahsam48@gmail.com>
2026-09-02 22:34:43 +0000

    validate(auth signup) : used express validator for validating fields for signup

---
+++ src/middleware/authValidation.js
@@ -0,0 +1,50 @@
+const { body } = require("express-validator");
+
+const validateAuth = [
+  body("firstName")
+    .trim()
+    .notEmpty()
+    .withMessage("First name is required")
+    .bail()
+    .isString()
+    .withMessage("First name must be a string")
+    .isLength({ min: 2, max: 50 })
+    .withMessage("First name character length must be between 2 and 50"),
+
+  body("lastName")
+    .trim()
+    .notEmpty()
+    .withMessage("Last name is required")
+    .bail()
+    .isString()
+    .withMessage("Last name must be a string")
+    .isLength({ min: 2, max: 50 })
+    .withMessage("Last name character length must be between 2 and 50"),
+
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Email is required")
+    .bail()
+    .isEmail()
+    .withMessage("email not valid")
+    .normalizeEmail(),
+
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .bail()
+    .isStrongPassword()
+    .withMessage(
+      "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one symbol.l",
+    ),
+
+  body("confirmPassword")
+    .notEmpty()
+    .withMessage("Confirm password is required")
+    .bail()
+    .custom((value, { req }) => value === req.body.password)
+    .withMessage("Passwords do not match"),
+];
+
+module.exports = validateAuth;
