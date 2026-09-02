2669a1626455fd11e633d34d0ec9c0dfec3ea2f4
inclined4X <jezemiahsam48@gmail.com>
2026-09-02 02:33:58 +0000

    feat: add Users model with validation for email, names, role, and status

---
+++ src/models/userModel.js
@@ -0,0 +1,54 @@
+const { default: mongoose, Schema } = require("mongoose");
+
+const usersSchema = new Schema(
+  {
+    email: {
+      type: String,
+      lowercase: true,
+      trim: true,
+      unique: true,
+      match: [
+        /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+        "Please provide a valid email address",
+      ],
+      required: [true, "email is required"],
+    },
+
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+
+    firstName: {
+      type: String,
+      minLength: [2, "First name must be at least 2 characters"],
+      maxLength: [50, "First name must not be more than 50 characters"],
+      trim: true,
+      required: true,
+    },
+
+    lastName: {
+      type: String,
+      minLength: [2, "Last name must be at least 2 characters"],
+      maxLength: [50, "Last name must not be more than 50 characters"],
+      trim: true,
+      required: true,
+    },
+
+    role: {
+      type: String,
+      enum: ["attendee", "organizer", "admin", "staff"],
+      required: true,
+    },
+
+    status: {
+      type: String,
+      enum: ["active", "suspended", "deactivated"],
+      default: "active",
+    },
+  },
+  { timestamps: true },
+);
+
+const Users = mongoose.model("Users", usersSchema);
+module.exports = Users;
