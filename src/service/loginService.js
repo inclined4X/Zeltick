05fd2a4022c779cedb33ca8dbc1e44097ea2be92
inclined4X <jezemiahsam48@gmail.com>
2026-09-03 02:04:21 +0000

    feat: added login service with credential validation
    Implemented a new login service that authenticates users by email
    and password. The service:

---
+++ src/service/loginService.js
@@ -0,0 +1,27 @@
+const { argon2 } = require("argon2");
+const AppError = require("../errors/appError");
+const userRepository = require("../repositories/userRepository");
+
+const login = async (credentials) => {
+  const { email, password } = credentials;
+
+  const user = await userRepository.findUserByEmail(email);
+
+  if (!user) {
+    throw new AppError("Invalid credentials!", 401);
+  }
+
+  const passwordMatch = await argon2d.verify(user.passwordHash, password);
+
+  if (!passwordMatch) {
+    throw new AppError("Invalid credentials", 401);
+  }
+
+  return {
+    id: user.id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    roles: user.roles,
+  };
+};
