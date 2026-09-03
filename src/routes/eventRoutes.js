542cbbe02cec6a81fc52a1c5549924292698baab
inclined4X <jezemiahsam48@gmail.com>
2026-09-03 23:33:39 +0000

    feat(events): added authentication and organizer authorization to event creation route

--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -2,8 +2,17 @@ const express = require("express");
 const validateEvent = require("../middleware/eventValidation");
 const validate = require("../middleware/validate");
 const eventController = require("../controllers/eventController");
+const authourize = require("../middleware/authourize");
+const authenticate = require("../middleware/authenticate");
 const router = express.Router();
 
-router.post("/", validateEvent, validate, eventController);
+router.post(
+  "/",
+  validateEvent,
+  validate,
+  authenticate,
+  authourize("organizer"),
+  eventController,
+);
 
 module.exports = router;
