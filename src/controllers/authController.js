1e2ec38d43cc2ed768f8650ab752cf2c11fedcd2
inclined4X <jezemiahsam48@gmail.com>
2026-09-03 02:20:45 +0000

    feat(auth): added login controller and fix argon2 import in loginService

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,5 @@
 const signupService = require("../service/signupService");
+const loginService = require("../service/loginService");
 
 const signup = async (req, res, next) => {
   try {
@@ -10,4 +11,14 @@ const signup = async (req, res, next) => {
   }
 };
 
-module.exports = signup;
+const login = async (req, res, next) => {
+  try {
+    const user = await loginService.login(req.body);
+
+    return res.status(200).json({ status: "success", data: user });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { signup, login };
--- src/service/loginService.js
+++ src/service/loginService.js
@@ -1,4 +1,4 @@
-const { argon2 } = require("argon2");
+const argon2 = require("argon2");
 const AppError = require("../errors/appError");
 const userRepository = require("../repositories/userRepository");
 
@@ -11,17 +11,21 @@ const login = async (credentials) => {
     throw new AppError("Invalid credentials!", 401);
   }
 
-  const passwordMatch = await argon2d.verify(user.passwordHash, password);
+  const passwordMatch = await argon2.verify(user.passwordHash, password);
 
   if (!passwordMatch) {
     throw new AppError("Invalid credentials", 401);
   }
 
   return {
-    id: user.id,
+    id: user._id,
     firstName: user.firstName,
     lastName: user.lastName,
     email: user.email,
     roles: user.roles,
   };
+};
+
+module.exports = {
+  login,
 };
