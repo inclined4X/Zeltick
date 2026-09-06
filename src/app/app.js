70fcb570cdda25f7e6907be0ad356c4352272ded
inclined4X <jezemiahsam48@gmail.com>
2026-09-06 02:58:41 +0000

    feat(cors): added CORS middleware for frontend dev server origin. this allows the frontend to call the api and send cookies which is required for authentication flows

--- src/app/app.js
+++ src/app/app.js
@@ -7,9 +7,18 @@ const session = require("express-session");
 const { sessionSecret, mongodbUri } = require("../config/env");
 const { default: MongoStore } = require("connect-mongo");
 const { httpLogger } = require("../utils/logger");
+const cors = require("cors");
 const app = express();
 
 app.use(httpLogger);
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 
 app.use(
