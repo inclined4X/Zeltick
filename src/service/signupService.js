4be7dadb0b025ccb9a0fe24c934711c5b30c0d82
inclined4X <jezemiahsam48@gmail.com>
2026-09-08 17:34:18 +0000

    feat(signup): hashed verification token during user registration

--- src/service/signupService.js
+++ src/service/signupService.js
@@ -1,6 +1,8 @@
 const AppError = require("../errors/appError");
 const userRepository = require("../repositories/userRepository");
 const argon2 = require("argon2");
+const crypto = require("crypto");
+const tokenGenerate = require("../utils/token");
 
 const signup = async (userData) => {
   const { firstName, lastName, email, password } = userData;
@@ -13,6 +15,11 @@ const signup = async (userData) => {
   const passwordHash = await argon2.hash(password, {
     type: argon2.argon2id,
   });
+
+  const tokenHash = crypto
+    .createHash("sha256")
+    .update(tokenGenerate)
+    .digest("hex");
 
   const newUser = await userRepository.createUser({
     firstName,
