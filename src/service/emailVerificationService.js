dacf95346bba021dc2b8887843c4335bad45357d
inclined4X <jezemiahsam48@gmail.com>
2026-09-09 14:29:15 +0000

    looked up with the hashed token sent a message if it can't be found

--- src/service/emailVerificationService.js
+++ src/service/emailVerificationService.js
@@ -1,5 +1,16 @@
-const crypto = require(crypto);
+const crypto = require("crypto");
+const userRepository = require("../repositories/userRepository");
+const AppError = require("../errors/appError");
 
-const verifyEmail = async (token) => {
-  const tokenHash = crypto.createHash("sha256").update(token).digest("hex");
+const verifyEmail = async (tokenFromUrl) => {
+  const tokenFromUrlHashed = crypto
+    .createHash("sha256")
+    .update(tokenFromUrl)
+    .digest("hex");
+
+  const userWithToken =
+    await userRepository.findUserByVerificationTokenHash(tokenFromUrlHashed);
+  if (!userWithToken) {
+    throw new AppError("Invalid or expired verification token", 401);
+  }
 };
