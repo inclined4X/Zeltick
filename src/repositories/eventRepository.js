f9f262c4153af6f41e7498f01bf78d51a2ad545d
inclined4X <jezemiahsam48@gmail.com>
2026-09-10 16:04:50 +0000

    feat: added findPublishedEvents to event repository

--- src/repositories/eventRepository.js
+++ src/repositories/eventRepository.js
@@ -4,4 +4,8 @@ const eventRepository = async (eventData) => {
   return await Event.create(eventData);
 };
 
+const findPublishedEvents = async () => {
+  return await Event.find({ status: "PUBLISHED", wasEverPublished: true });
+};
+
 module.exports = eventRepository;
