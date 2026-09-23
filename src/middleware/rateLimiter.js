e362d49c979c89b09ed3e71eef44d5be338b38d1
inclined4X <jezemiahsam48@gmail.com>
2026-09-23 19:35:02 +0000

    added rate limiter handler so that error are passsed to the global error handler without bypassing it

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -1,32 +1,45 @@
 const { rateLimit } = require("express-rate-limit");
 
+const AppError = require("../errors/appError");
+
 const loginLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   limit: 5,
-  message: { error: "Too many login attempts, please try again later." },
   standardHeaders: true,
   legacyHeaders: false,
   skipSuccessfulRequests: true,
+  handler: (req, res, next) => {
+    next(new AppError("Too many login attempts, please try again later.", 429));
+  },
 });
 
 const signupLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   limit: 5,
-  message: { error: "Too many signup attempts, please try again later" },
   standardHeaders: true,
   legacyHeaders: false,
   skipSuccessfulRequests: false,
+  handler: (req, res, next) => {
+    next(
+      new AppError("Too many signup attempts, please try again later.", 429),
+    );
+  },
 });
 
 const resendLimiter = rateLimit({
   windowMs: 5 * 60 * 1000,
   limit: 3,
-  message: {
-    error: "Too many verification resend requests, please try again later",
-  },
   standardHeaders: true,
   legacyHeaders: false,
   skipSuccessfulRequests: false,
+  handler: (req, res, next) => {
+    next(
+      new AppError(
+        "Too many verification resend requests, please try again later.",
+        429,
+      ),
+    );
+  },
 });
 
 module.exports = { loginLimiter, signupLimiter, resendLimiter };
